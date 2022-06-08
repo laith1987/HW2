2de28fdc0ac71ca076405c2f8b70ebd24af92047
laith1987 <107152487+laith1987@users.noreply.github.com>
2022-06-09 00:44:09 +0300

    Add files via upload

---
+++ LaTow/LaTow/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace LaTow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtusernae = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtconpass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btnUpdata = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtusernae
+            // 
+            this.txtusernae.Location = new System.Drawing.Point(114, 94);
+            this.txtusernae.Name = "txtusernae";
+            this.txtusernae.Size = new System.Drawing.Size(197, 20);
+            this.txtusernae.TabIndex = 0;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(114, 135);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(197, 20);
+            this.txtpassword.TabIndex = 0;
+            // 
+            // txtconpass
+            // 
+            this.txtconpass.Location = new System.Drawing.Point(114, 176);
+            this.txtconpass.Name = "txtconpass";
+            this.txtconpass.Size = new System.Drawing.Size(197, 20);
+            this.txtconpass.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "user name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "con_ password";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(388, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(373, 158);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(235, 250);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 3;
+            this.btn_add.Text = "add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(335, 250);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_delete.TabIndex = 3;
+            this.btn_delete.Text = "delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btnUpdata
+            // 
+            this.btnUpdata.Location = new System.Drawing.Point(446, 250);
+            this.btnUpdata.Name = "btnUpdata";
+            this.btnUpdata.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdata.TabIndex = 3;
+            this.btnUpdata.Text = "Updata";
+            this.btnUpdata.UseVisualStyleBackColor = true;
+            this.btnUpdata.Click += new System.EventHandler(this.btnUpdata_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 357);
+            this.Controls.Add(this.btnUpdata);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtconpass);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusernae);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtusernae;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtconpass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btnUpdata;
+    }
+}
+
---
+++ LaTow/LaTow/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaTow
+{
+    public partial class Form1 : Form
+    {
+        TowLayer layer;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+           layer = new TowLayer();//inta
+            layer.username=txtusernae.ToString();
+            layer.pass = txtpassword.ToString();
+            layer.conopass = txtconpass.ToString();
+            layer.add();
+            MessageBox.Show("add successfully");
+
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            layer = new TowLayer();//inta
+            layer.username = txtusernae.ToString();
+            layer.del();
+            MessageBox.Show("del successfully");
+        }
+
+        private void btnUpdata_Click(object sender, EventArgs e)
+        {
+
+            layer = new TowLayer();//inta
+            layer.username = txtusernae.ToString();
+            layer.pass = txtpassword.ToString();
+            layer.conopass = txtconpass.ToString();
+            layer.edit();
+            MessageBox.Show("edit successfully");
+
+        }
+    }
+    public class oneLayer
+    {
+        static SqlConnection Connection = new SqlConnection("data source = DESKTOP-DELL\\SQLEXPRESS; initial catalog=IM; persist security info=True; Integrated Security = SSPI;");
+        public static void add(TowLayer lib)
+        {
+
+            SqlCommand cmd = new SqlCommand("insert into  [user] (,[User],[password],[conpassword])Values(@User,@password,@conpassword)", Connection);
+            cmd.Parameters.AddWithValue("@User", lib.username);
+            cmd.Parameters.AddWithValue("@password", lib.pass);
+            cmd.Parameters.AddWithValue("@conpassword", lib.conopass);
+            Connection.Open();
+            cmd.ExecuteNonQuery();
+            Connection.Close();
+        }
+        public static void edit(TowLayer lib)
+        {
+            SqlCommand cmd = new SqlCommand("Update user set ,[password]=@password,[conpassword]=@conpassword where [User]=@User", Connection);
+            
+            cmd.Parameters.AddWithValue("@Name_User", lib.username);
+            cmd.Parameters.AddWithValue("@Password_User", lib.pass);
+            cmd.Parameters.AddWithValue("@Email_User", lib.conopass);
+            Connection.Open();
+            cmd.ExecuteNonQuery();
+            Connection.Close();
+
+        }
+        public static void delete(TowLayer lib)
+        {
+            SqlCommand cmd = new SqlCommand("delete from [user] where [User]=@User", Connection);
+            Connection.Open();
+            cmd.ExecuteNonQuery();
+            Connection.Close();
+
+        }
+    }
+    public class TowLayer
+    {
+        
+        public string username { get; set; }
+        public string pass { get; set; }
+        public string conopass { get; set; }
+        public void add()
+        {
+            oneLayer.add(this);
+        }public void edit()
+        {
+            oneLayer.edit(this );
+        }public void del()
+        {
+            oneLayer.delete(this);
+        }
+    }
+}
